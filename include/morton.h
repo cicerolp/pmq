33cb75876f534bd1f9244956f897cbc4a10e3d6c
Julio Toss <jutoss@gmail.com>
2016-09-05 17:31:43 -0300

    Added 64-bit morton decode functions

--- include/morton.h
+++ include/morton.h
@@ -286,15 +286,33 @@ void mortonDecode_RAM(uint32_t i, uint32_t& x, uint32_t& y, uint32_t& z){
     return ;
 }
 
+
+/* ********** 64 bit versions ********************/
+
+uint32_t m_undilate_2(uint64_t t) {
+  t = (t | (t >> 1)) & 0x3333333333333333;
+  t = (t | (t >> 2)) & 0x0F0F0F0F0F0F0F0F;
+  t = (t | (t >> 4)) & 0x00FF00FF00FF00FF;
+  t = (t | (t >> 8)) & 0x0000FFFF0000FFFF;
+  t = (t | (t >> 16)) & 0x00000000FFFFFFFF;
+  return((uint32_t) t);
+}
+
+
+/**
+ * @brief mortonDecode_RAM  decode a morton code to its (x,y) index;
+ * @param i [IN] A 64-bit morton code
+ * @param x [OUT] x coordinates
+ * @param y [OUT] y coordinates
+ */
 inline
-void mortonDecode_RAM(uint32_t i, uint32_t& x, uint32_t& y){
+void mortonDecode_RAM(uint64_t i, uint32_t& x, uint32_t& y){
     x = 0; y = 0;
-    x = m_undilate_2(i & 0x55555555);
-    y = m_undilate_2((i>>1) & 0x55555555);
+    x = m_undilate_2(i & 0x5555555555555555);
+    y = m_undilate_2((i>>1) & 0x5555555555555555 );
     return ;
 }
 
-// 64 bit versions
 inline
 uint64_t m_dilate_2(uint32_t t){
     uint64_t r = t;
