ffdfd399ba59364ffbc96b32be53f581e9d6eafb
Julio Toss <jutoss@gmail.com>
2016-09-06 17:28:15 -0300

    UPD: quatree update

--- include/SpatialElement.cpp
+++ include/SpatialElement.cpp
@@ -3,6 +3,8 @@
 #include "mercator_util.h"
 #include <vector>
 
+#include <pma/utils/debugMacros.h>
+
 SpatialElement::SpatialElement(const spatial_t& tile) {
    el = tile;
    beg = NULL;
@@ -11,25 +13,29 @@ SpatialElement::SpatialElement(const spatial_t& tile) {
 
 /**
  * @brief SpatialElement::update inserts a list of element in a quatree
- * @param range
+ * @param range A list of elements that have been modified in the quadtree (added or updated) with their new ranges in the pma.
+ *
+ * @note we don't support deletes;
  * @return
  */
 uint32_t SpatialElement::update(map_t &range) {
 
-  if (el.z == 24)
+  if (el.z == g_Quadtree_Depth -1 )
       return 0;
 
   std::vector<map_t> quads(4);
+
   for (auto& m : range){
       int y = mercator_util::lat2tiley(m.first.lat,el.z);
       int x = mercator_util::lon2tilex(m.first.lgt,el.z);
       int q = mercator_util::index(x,y);
 
-      quads[q].emplace(m);
+      (quads[q]).emplace(m);
   }
 
-
-
+  //reset pointers of this node
+  beg = NULL;
+  end = NULL;
 
   for (int i=0 ; i < 4 ; i++ ){
       if (quads[i].empty()) continue;
@@ -39,10 +45,11 @@ uint32_t SpatialElement::update(map_t &range) {
         _container[i] = std::make_unique<SpatialElement> (spatial_t(tile.first,tile.second,el.z+1));
       }
 
-      if (beg == NULL ){
+      if (beg == NULL ){ // points to beggining of the first quadrant
           beg = (* (quads[i].begin())).second.first;
       }
 
+      //points to the end of the last quadrant
       end = (* (quads[i].rbegin())).second.second;
 
       _container[i]->update(quads[i]);
@@ -50,9 +57,10 @@ uint32_t SpatialElement::update(map_t &range) {
 
   }
 
-//  std::cout << "("<< el << ") " << "[" << (void * ) beg << " - " << (void*) end << "]" << std::endl;
+  std::cout << "("<< el << ") " << "[" << (void * ) beg << " - " << (void*) end << "]" << std::endl;
 
 }
+
 /*
 void SpatialElement::query_tile(const spatial_t& tile, uint64_t resolution, binned_ctn& subset, uint64_t zoom) const {
    const spatial_t& value = (*reinterpret_cast<const spatial_t*>(&el.value));
