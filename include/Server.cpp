6561c45eae73a7fe3aa05e20bb64e5184173b088
Cícero Pahins <cicerolp@gmail.com>
2016-09-28 17:29:31 +0000

    add support to WebSockets (client-side)

--- include/Server.cpp
+++ include/Server.cpp
@@ -3,8 +3,7 @@
 #include "Query.h"
 #include "PMAInstance.h"
 
-void Server::run(server_opts opts)
-{
+void Server::run(server_opts opts) {
    Server::getInstance().nds_opts = opts;
 
    setvbuf(stdout, NULL, _IOLBF, 0);
@@ -12,92 +11,70 @@ void Server::run(server_opts opts)
 
    mg_mgr_init(&Server::getInstance().mgr, nullptr);
    Server::getInstance().nc =
-      mg_bind(&Server::getInstance().mgr, std::to_string(Server::getInstance().nds_opts.port).c_str(), handler);
+         mg_bind(&Server::getInstance().mgr, std::to_string(Server::getInstance().nds_opts.port).c_str(), handler);
 
    mg_set_protocol_http_websocket(Server::getInstance().nc);
 
    Server::getInstance().http_server_opts.document_root = "../WebContent";
 
-   if (Server::getInstance().nds_opts.multithreading)
-   {
+   if (Server::getInstance().nds_opts.multithreading) {
       mg_enable_multithreading(Server::getInstance().nc);
    }
 
-   while (Server::getInstance().running)
-   {
+   while (Server::getInstance().running) {
       mg_mgr_poll(&Server::getInstance().mgr, 1);
       Server::getInstance().broadcast();
    }
    mg_mgr_free(&Server::getInstance().mgr);
 }
 
-void Server::handler(struct mg_connection* nc, int ev, void* ev_data)
-{
-   switch (ev)
-   {
-   case MG_EV_HTTP_REQUEST:
-      {
+void Server::handler(struct mg_connection* nc, int ev, void* ev_data) {
+   switch (ev) {
+      case MG_EV_HTTP_REQUEST: {
          struct http_message* hm = (struct http_message *) ev_data;
          std::string uri(hm->uri.p, hm->uri.len);
 
-         try
-         {
+         try {
             std::vector<std::string> tokens = string_util::split(uri, "[/]+");
 
-            if (tokens.size() <= 1)
-            {
+            if (tokens.size() <= 1) {
                mg_serve_http(nc, hm, Server::getInstance().http_server_opts);
-            }
-            else if (tokens[1] == "rest" && tokens.size() >= 3)
-            {
-               if (tokens.size() >= 5 && tokens[2] == "query")
-               {
+            } else if (tokens[1] == "rest" && tokens.size() >= 3) {
+               if (tokens.size() >= 5 && tokens[2] == "query") {
                   printJson(nc, PMAInstance::getInstance().query(Query(tokens)));
-               }
-               else
-               {
+               } else {
                   printJson(nc, "[]");
                }
-            }
-            else
-            {
+            } else {
                mg_serve_http(nc, hm, Server::getInstance().http_server_opts);
             }
-         }
-         catch (...)
-         {
+         } catch (...) {
             std::cout << uri << std::endl;
             printJson(nc, "[]");
          }
          break;
       }
-
-   case MG_EV_WEBSOCKET_HANDSHAKE_DONE:
-      {  
+      case MG_EV_WEBSOCKET_HANDSHAKE_DONE: {
          // new websocket connection
          Server::getInstance().mutex.lock();
          Server::getInstance().up_to_date.emplace(nc, false);
          Server::getInstance().mutex.unlock();
          break;
       }
-   case MG_EV_WEBSOCKET_FRAME:
-      {
+      case MG_EV_WEBSOCKET_FRAME: {
          struct websocket_message* wm = (struct websocket_message *) ev_data;
          std::string uri((char *)wm->data, wm->size);
-         
+
          if (uri == "update") {
             Server::getInstance().mutex.lock();
             Server::getInstance().up_to_date[nc] = false;
             Server::getInstance().mutex.unlock();
          }
-         
          break;
       }
-   case MG_EV_CLOSE:
-      {
+      case MG_EV_CLOSE: {
          // disconnect
-         if (is_websocket(nc))
-         {
+         if (is_websocket(nc)) {
             Server::getInstance().mutex.lock();
             Server::getInstance().up_to_date.erase(nc);
             Server::getInstance().mutex.unlock();
@@ -117,12 +94,12 @@ void Server::renew_data() {
 
 void Server::broadcast() {
    static const std::string msg = "renew";
-   
+
    mutex.lock();
-   for (auto& pair : up_to_date) {      
+   for (auto& pair : up_to_date) {
       mg_connection* conn = pair.first;
 
-      if (pair.second == false) {         
+      if (pair.second == false) {
          mg_send_websocket_frame(conn, WEBSOCKET_OP_TEXT, msg.c_str(), msg.size());
          pair.second = true;
       }
@@ -130,10 +107,8 @@ void Server::broadcast() {
    mutex.unlock();
 }
 
-void Server::printText(mg_connection* conn, const std::string& content)
-{
-   if (Server::getInstance().nds_opts.cache)
-   {
+void Server::printText(mg_connection* conn, const std::string& content) {
+   if (Server::getInstance().nds_opts.cache) {
       mg_printf(conn,
                 "HTTP/1.1 200 OK\r\n"
                 "Content-Type: text/plain\r\n"
@@ -144,9 +119,7 @@ void Server::printText(mg_connection* conn, const std::string& content)
                 "\r\n"
                 "%s",
                 (int)content.size(), content.c_str());
-   }
-   else
-   {
+   } else {
       mg_printf(conn,
                 "HTTP/1.1 200 OK\r\n"
                 "Content-Type: text/plain\r\n"
@@ -160,10 +133,8 @@ void Server::printText(mg_connection* conn, const std::string& content)
    }
 }
 
-void Server::printJson(mg_connection* conn, const std::string& content)
-{
-   if (Server::getInstance().nds_opts.cache && content != "[]")
-   {
+void Server::printJson(mg_connection* conn, const std::string& content) {
+   if (Server::getInstance().nds_opts.cache && content != "[]") {
       mg_printf(conn,
                 "HTTP/1.1 200 OK\r\n"
                 "Content-Type: application/json\r\n"
@@ -174,9 +145,7 @@ void Server::printJson(mg_connection* conn, const std::string& content)
                 "\r\n"
                 "%s",
                 (int)content.size(), content.c_str());
-   }
-   else
-   {
+   } else {
       mg_printf(conn,
                 "HTTP/1.1 200 OK\r\n"
                 "Content-Type: application/json\r\n"
