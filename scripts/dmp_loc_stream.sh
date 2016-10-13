0757e9becbaa51a0d25d91a223cde9512262e61b
Julio Toss <jutoss@gmail.com>
2016-10-13 10:41:18 -0300

    UPD: save stderr from twitter stream

--- scripts/dmp_loc_stream.sh
+++ scripts/dmp_loc_stream.sh
@@ -5,6 +5,7 @@
 while true
 do
   output=$2_$(date +%F_%Hh%M).json
+  stderr=$2_$(date +%F_%Hh%M)_err.json
   echo $output
-  timeout 1h python ./getStreams.py -e statuses/filter -p locations=$1 >> $output
+  timeout 1h python ./getStreams.py -e statuses/filter -p locations=$1 >> $output 2>> $stderr
 done
--- scripts/dmp_track_stream.sh
+++ scripts/dmp_track_stream.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+#usage ./dmp_track_stream.sh "pizza" "filename"
+
 while true
 do
   output=$2_$(date +%F_%Hh%M).json
+  stderr=$2_$(date +%F_%Hh%M)_err.json
   echo $output
-  timeout 1h python ./getStreams.py -e statuses/filter -p track="$1" >> $output
+  timeout 1h python ./getStreams.py -e statuses/filter -p track="$1" >> $output 2>> $stderr
 done
