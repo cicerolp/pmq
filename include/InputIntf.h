5fd10d5b1e5ed8dc28ba02175aa5f57d5ae34a79
Julio Toss <jutoss@gmail.com>
2016-10-19 12:53:28 -0200

    UPD: random twitter initialization

--- include/InputIntf.h
+++ include/InputIntf.h
@@ -2,6 +2,7 @@
 
 #include "stde.h"
 #include "types.h"
+#include <random>
 
 namespace input {
    inline std::vector<elttype> load(const std::string& fname, int mCodeSize) {
@@ -37,20 +38,26 @@ namespace input {
    inline std::vector<elttype> dist_random(unsigned int nb_el, long rseed) {
       std::vector<elttype> keyVal_vec;
 
+      std::mt19937 gen(rseed);
+      /*
+      std::random_device rd;
+
       if (rseed != 0) {
-         srand(rseed);
+         gen.seed(rseed);
       } else {
-         long seed = time(NULL);
-         fprintf(stdout, "Seed : %ld\n", seed);
-         srand(seed);
+         gen.seed(rd);
+         fprintf(stdout, "Random seed ");
       }
+*/
+
+      std::uniform_real_distribution<> lon(-180, 180);
+      std::uniform_real_distribution<> lat(-85, 85);
 
       for (int i = 0; i < nb_el; i++) {
-         elttype el;
-         el.key = rand() % nb_el;
-         el.value = tweet_t();
-
-         keyVal_vec.push_back(el);
+         tweet_t el;
+         el.longitude = lon(gen);
+         el.latitude = lat(gen);
+         keyVal_vec.emplace_back(el,25);
       }
 
       return keyVal_vec;
