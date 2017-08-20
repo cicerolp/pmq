5c3bb59482cfc026f75e62dfceb8cf7d18b015de
Cícero Pahins <cicerolp@gmail.com>
2017-08-20 08:59:08 -0300

    btree fully integrated

---
+++ include/BTreeCtn.cpp
@@ -0,0 +1,140 @@
+//
+// Created by cicerolp on 8/20/17.
+//
+
+#include "BTreeCtn.h"
+
+duration_t BTreeCtn::create(uint32_t size) {
+  Timer timer;
+  timer.start();
+
+  _size = size;
+
+  _btree = std::make_unique<stx::btree<key, data>>();
+
+  timer.stop();
+  return {duration_info("create", timer)};
+}
+
+duration_t BTreeCtn::insert(std::vector<elttype> batch) {
+  Timer timer;
+  timer.start();
+
+  for (const auto &elt: batch) {
+    _btree->insert(elt.key, elt.value);
+  }
+
+  timer.stop();
+  return {duration_info("insert", timer)};
+}
+duration_t BTreeCtn::scan_at_region(const region_t &region, scantype_function __apply) {
+  Timer timer;
+  timer.start();
+
+  scan_btree_at_region(get_parent_quadrant(region), region, __apply);
+
+  timer.stop();
+  return {duration_info("scan_at_region", timer)};
+}
+
+void BTreeCtn::scan_btree_at_region(const code_t &el, const region_t &region, scantype_function __apply) {
+  if (el.z > region.z) return;
+
+  region_t::overlap overlap = region.test(el);
+
+  if (overlap == region_t::full) {
+    auto lower_it = _btree->lower_bound(el.min_code);
+    auto upper_it = _btree->lower_bound(el.max_code);
+
+    for (auto it = lower_it; it != upper_it; ++it) {
+      __apply((*it).second);
+    }
+
+  } else if (overlap != region_t::none) {
+    // break code into four
+    uint64_t code = el.code << 2;
+
+    scan_btree_at_region(code_t(code | 0, (uint32_t) (el.z + 1)), region, __apply);
+    scan_btree_at_region(code_t(code | 1, (uint32_t) (el.z + 1)), region, __apply);
+    scan_btree_at_region(code_t(code | 2, (uint32_t) (el.z + 1)), region, __apply);
+    scan_btree_at_region(code_t(code | 3, (uint32_t) (el.z + 1)), region, __apply);
+  }
+}
+duration_t BTreeCtn::apply_at_tile(const region_t &region, applytype_function __apply) {
+  Timer timer;
+  timer.start();
+
+  apply_btree_at_tile(get_parent_quadrant(region), region, __apply);
+
+  timer.stop();
+  return {duration_info("apply_at_tile", timer)};
+}
+
+void BTreeCtn::apply_btree_at_tile(const code_t &el, const region_t &region, applytype_function __apply) {
+  if (el.z >= 25 || (int) el.z - (int) region.z > 8) return;
+
+  region_t::overlap overlap = region.test(el);
+
+  if ((int) el.z - (int) region.z < 8) {
+    if (overlap != region_t::none) {
+      // break morton code into four
+      uint64_t code = el.code << 2;
+
+      apply_btree_at_tile(code_t(code | 0, (uint32_t) (el.z + 1)), region, __apply);
+      apply_btree_at_tile(code_t(code | 1, (uint32_t) (el.z + 1)), region, __apply);
+      apply_btree_at_tile(code_t(code | 2, (uint32_t) (el.z + 1)), region, __apply);
+      apply_btree_at_tile(code_t(code | 3, (uint32_t) (el.z + 1)), region, __apply);
+    }
+  } else if (overlap == region_t::full) {
+    uint32_t count = 0;
+
+    auto lower_it = _btree->lower_bound(el.min_code);
+    auto upper_it = _btree->lower_bound(el.max_code);
+
+    while (lower_it++ != upper_it) {
+      ++count;
+    }
+
+    if (count) {
+      __apply(el, count);
+    }
+  }
+}
+duration_t BTreeCtn::apply_at_region(const region_t &region, applytype_function __apply) {
+  Timer timer;
+  timer.start();
+
+  apply_btree_at_region(get_parent_quadrant(region), region, __apply);
+
+  timer.stop();
+  return {duration_info("apply_at_region", timer)};
+}
+
+void BTreeCtn::apply_btree_at_region(const code_t &el, const region_t &region, applytype_function __apply) {
+  if (el.z > region.z) return;
+
+  region_t::overlap overlap = region.test(el);
+
+  if (overlap == region_t::full) {
+    uint32_t count = 0;
+
+    auto lower_it = _btree->lower_bound(el.min_code);
+    auto upper_it = _btree->lower_bound(el.max_code);
+
+    while (lower_it++ != upper_it) {
+      ++count;
+    }
+
+    if (count) {
+      __apply(el, count);
+    }
+  } else if (overlap != region_t::none) {
+    // break morton code into four
+    uint64_t code = el.code << 2;
+
+    apply_btree_at_region(code_t(code | 0, (uint32_t) (el.z + 1)), region, __apply);
+    apply_btree_at_region(code_t(code | 1, (uint32_t) (el.z + 1)), region, __apply);
+    apply_btree_at_region(code_t(code | 2, (uint32_t) (el.z + 1)), region, __apply);
+    apply_btree_at_region(code_t(code | 3, (uint32_t) (el.z + 1)), region, __apply);
+  }
+}
---
+++ include/BTreeCtn.h
@@ -0,0 +1,72 @@
+//
+// Created by cicerolp on 8/20/17.
+//
+
+#pragma once
+
+#include "GeoCtnIntf.h"
+
+class BTreeCtn : public GeoCtnIntf {
+ public:
+  BTreeCtn(int argc, char *argv[]) : GeoCtnIntf() {
+
+  }
+  virtual ~BTreeCtn() = default;
+
+  duration_t create(uint32_t size) override;
+
+  duration_t insert(std::vector<elttype> batch) override;
+
+  duration_t insert_rm(std::vector<elttype> batch, std::function<int(const void *)> is_removed) override {
+    return GeoCtnIntf::insert_rm(batch, is_removed);
+  }
+
+  duration_t scan_at_region(const region_t &region, scantype_function __apply) override;
+  void scan_btree_at_region(const code_t &el, const region_t &region, scantype_function __apply);
+
+  duration_t apply_at_tile(const region_t &region, applytype_function __apply) override;
+  void apply_btree_at_tile(const code_t &el, const region_t &region, applytype_function __apply);
+
+  duration_t apply_at_region(const region_t &region, applytype_function __apply) override;
+  void apply_btree_at_region(const code_t &el, const region_t &region, applytype_function __apply);
+
+  duration_t topk_search(const region_t &region, topk_t &topk, scantype_function __apply) override {
+    return GeoCtnIntf::topk_search(region, topk, __apply);
+  }
+  std::string name() const override {
+    static auto name_str = "BTree";
+    return name_str;
+  }
+
+  inline code_t get_parent_quadrant(const region_t &region) const {
+    uint64_t mask = region.code0 ^region.code1;
+
+    mask |= mask >> 32;
+    mask |= mask >> 16;
+    mask |= mask >> 8;
+    mask |= mask >> 4;
+    mask |= mask >> 2;
+    mask |= mask >> 1;
+
+    mask = (~mask);
+
+    uint64_t prefix = mask & region.code1;
+
+    uint32_t depth_diff = 0;
+    while ((mask & 3) != 3) {
+      depth_diff++;
+      mask = mask >> 2;
+    }
+
+    prefix = prefix >> (depth_diff * 2);
+
+    return code_t(prefix, region.z - depth_diff);
+  }
+
+ protected:
+  typedef uint64_t key;
+  typedef valuetype data;
+
+  uint32_t _size;
+  std::unique_ptr<stx::btree<key, data>> _btree;
+};
