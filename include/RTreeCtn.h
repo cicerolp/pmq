1dd3333727869ffd8de865e561642deb77dae54d
Cícero Pahins <cicerolp@gmail.com>
2017-09-23 22:33:57 -0300

    remove unused types and change conversion region_t -> boost::box in rtree

--- include/RTreeCtn.h
+++ include/RTreeCtn.h
@@ -87,16 +87,8 @@ class RTreeCtn : public GeoCtnIntf<T> {
     Timer timer;
     timer.start();
 
-    // longitude
-    float xmin = mercator_util::tilex2lon(region.x0, region.z);
-    float xmax = mercator_util::tilex2lon(region.x1 + 1, region.z);
-
-    // latitude
-    float ymin = mercator_util::tiley2lat(region.y1 + 1, region.z);
-    float ymax = mercator_util::tiley2lat(region.y0, region.z);
-
     // convert from region_t to boost:box
-    box query_box(point(ymin, xmin), point(ymax, xmax));
+    box query_box(point(region.lat1, region.lon0), point(region.lat0, region.lon1));
 
     // temporary result
     std::vector<value> result;
@@ -114,16 +106,8 @@ class RTreeCtn : public GeoCtnIntf<T> {
     Timer timer;
     timer.start();
 
-    // longitude
-    float xmin = mercator_util::tilex2lon(region.x0, region.z);
-    float xmax = mercator_util::tilex2lon(region.x1 + 1, region.z);  // JULIO : why + 1 ??
-
-    // latitude
-    float ymin = mercator_util::tiley2lat(region.y1 + 1, region.z);
-    float ymax = mercator_util::tiley2lat(region.y0, region.z);
-
     // convert from region_t to boost:box
-    box query_box(point(ymin, xmin), point(ymax, xmax));
+    box query_box(point(region.lat1, region.lon0), point(region.lat0, region.lon1));
 
     // temporary result
     RTreeCtnCounter<value> result;
